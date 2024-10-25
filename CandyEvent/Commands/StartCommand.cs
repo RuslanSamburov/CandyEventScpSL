fe65243cce7e4bc39dcee9629380b144a8167dd4
Ruslan Samburov <samburov.com@gmail.com>
2024-10-25 18:47:10 +0300

    Исправлены баги

--- CandyEvent/Commands/StartCommand.cs
+++ CandyEvent/Commands/StartCommand.cs
@@ -1,4 +1,5 @@
 ﻿using CommandSystem;
+using Exiled.Permissions.Extensions;
 using System;
 
 namespace CandyEvent.Commands
@@ -11,6 +12,18 @@ namespace CandyEvent.Commands
 
         public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
         {
+            if (!sender.CheckPermission($"{MainCommand.Prefix}.{Command}"))
+            {
+                response = MainCommand.NotPermissionMessage;
+                return false;
+            }
+
+            if (Plugin.Singleton._candyCoroutine.IsRunning)
+            {
+                response = "Таймер запущен!";
+                return false;
+            }
+
             int? interval = null;
 
             if (arguments.Count > 0 && int.TryParse(arguments.At(0), out int parsedInterval))
@@ -19,7 +32,7 @@ namespace CandyEvent.Commands
             }
 
             Plugin.Singleton._management.StartCandyEvent(interval);
-            response = "Ивент запущен с интервалом " + interval + " сек";
+            response = $"Ивент запущен с интервалом {interval ?? Plugin.Singleton.Config.CandyInterval} сек";
             return true;
         }
     }
--- CandyEvent/Commands/StopCommand.cs
+++ CandyEvent/Commands/StopCommand.cs
@@ -1,4 +1,5 @@
 ﻿using CommandSystem;
+using Exiled.Permissions.Extensions;
 using System;
 
 namespace CandyEvent.Commands
@@ -11,6 +12,18 @@ namespace CandyEvent.Commands
 
         public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
         {
+            if (!sender.CheckPermission($"{MainCommand.Prefix}.{Command}"))
+            {
+                response = MainCommand.NotPermissionMessage;
+                return false;
+            }
+
+            if (!Plugin.Singleton._candyCoroutine.IsRunning)
+            {
+                response = "Ивент не запущен!";
+                return false;
+            }
+
             Plugin.Singleton._management.StopCandyEvent(); 
             response = "Ивент остановлен";
             return true;
